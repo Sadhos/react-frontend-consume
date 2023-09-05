c3fd846adb13965de0f38c2c5fd0a415f294b49b
Sadhos <nknthakur@gmail.com>
2023-09-05 19:44:05 +0530

    new aws

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-const baseURL_AWS = `http://myapiserver-env.eba-yp3ppedq.ap-south-1.elasticbeanstalk.com/`;
+const baseURL_AWS = `http://newserver-env.eba-jqigum7k.ap-south-1.elasticbeanstalk.com`;
 const baseURL_LOCAL = `http://localhost:8080`;
+const baseURL_DOCKER = `https://localhost`;
 export default axios.create({
     baseURL: baseURL_AWS,
     headers: {
